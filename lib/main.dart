10a3aad5adcf2fe2bc562945dc5afb6bb3c1275d
RealOshina <130570122+RealOshina@users.noreply.github.com>
2026-06-07 21:41:26 +0700

    v0.5.0 - Add grid number button

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:ui';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await windowManager.ensureInitialized();
+
+    if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
+        const WindowOptions windowOptions = WindowOptions(
+            size: Size(420, 600),
+            center: true,
+        );
+
+        //await windowManager.setSize(const Size(420, 600));
+        //await windowManager.setResizable(false);
+
+        //SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+        windowManager.waitUntilReadyToShow(windowOptions, () async {
+            await windowManager.show();
+            await windowManager.focus();
+        });
+    }
+
+    runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -35,55 +62,56 @@ class _MyHomePageState extends State<MyHomePage> {
                     Flexible(
                         child: 
                             Container(
-                                height: 80,
-                                padding: const EdgeInsets.symmetric(vertical: 50),
+                                height: 100,
                                 alignment: Alignment.topCenter,
                                 color: Color.fromARGB(255, 211, 211, 211),
                                 child: const Align(
                                     child:
                                         Text('Number', 
                                             textAlign: TextAlign.center, 
-                                            style: TextStyle(fontSize: 24)),
+                                            style: TextStyle(fontSize: 42)),
                                 ),
                             ),
                     ),
-                    Expanded(
+                    Flexible(
                         child:
                             Container(
-                                alignment: Alignment.topCenter,
+                                alignment: Alignment.center,
                                 //color: Color.fromARGB(255, 211, 211, 211),
                                 color: Colors.yellow,
-                                padding: const EdgeInsets.symmetric(vertical: 20),
+                                padding: const EdgeInsets.all(25),
+
                                 child: Align(
                                         child: GridView.count(
-                                            crossAxisCount: 3,
+                                            crossAxisCount: 4,
+                                            mainAxisSpacing: 10,
+                                            crossAxisSpacing: 10,
+                                            childAspectRatio: 1,
+
                                             shrinkWrap: true,
                                             physics: NeverScrollableScrollPhysics(),
 
                                             children: [
+                                                    // Row 1
                                                     ElevatedButton(
                                                         style: ElevatedButton.styleFrom(
                                                             backgroundColor: Colors.grey,
                                                             foregroundColor: Colors.black,
-                                                            elevation: 5,
                                                             shape: RoundedRectangleBorder(
                                                                 borderRadius: BorderRadius.circular(12),
                                                             ),
-                                                            minimumSize: const Size(50, 50),
-                                                            maximumSize: const Size(80, 80),
                                                         ),
 
                                                         onPressed:() {
                                                             print('1');
                                                         },
 
-                                                        child: const Text('1')
+                                                        child: const Text('1'),
                                                     ),
                                                     ElevatedButton(
                                                         style: ElevatedButton.styleFrom(
                                                             backgroundColor: Colors.orange,
                                                             foregroundColor: Colors.black,
-                                                            //elevation: 5,
                                                             shape: RoundedRectangleBorder(
                                                                 borderRadius: BorderRadius.circular(12),
                                                             ),
@@ -94,6 +122,222 @@ class _MyHomePageState extends State<MyHomePage> {
                                                         },
 
                                                         child: const Text('2')
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('3');
+                                                        },
+
+                                                        child: const Text('3')
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('+');
+                                                        },
+
+                                                        child: const Text('+')
+                                                    ),
+
+                                                    // Row 2
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.grey,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('4');
+                                                        },
+
+                                                        child: const Text('4'),
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('5');
+                                                        },
+
+                                                        child: const Text('5')
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('6');
+                                                        },
+
+                                                        child: const Text('6')
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('-');
+                                                        },
+
+                                                        child: const Text('-')
+                                                    ),
+
+                                                    // Row 3
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.grey,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('7');
+                                                        },
+
+                                                        child: const Text('7'),
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('8');
+                                                        },
+
+                                                        child: const Text('8')
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('9');
+                                                        },
+
+                                                        child: const Text('9')
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('*');
+                                                        },
+
+                                                        child: const Text('*')
+                                                    ),
+
+                                                    // Row 4
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.grey,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('C');
+                                                        },
+
+                                                        child: const Text('C'),
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('0');
+                                                        },
+
+                                                        child: const Text('0')
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('=');
+                                                        },
+
+                                                        child: const Text('=')
+                                                    ),
+                                                    ElevatedButton(
+                                                        style: ElevatedButton.styleFrom(
+                                                            backgroundColor: Colors.orange,
+                                                            foregroundColor: Colors.black,
+                                                            shape: RoundedRectangleBorder(
+                                                                borderRadius: BorderRadius.circular(12),
+                                                            ),
+                                                        ),
+
+                                                        onPressed:() {
+                                                            print('/');
+                                                        },
+
+                                                        child: const Text('/')
                                                     ),
                                             ],
                                         ),
